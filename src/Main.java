388f1e6c27bbf556c212344ba8500c72a627684a
vincenzodivita <divitavincenzo@icloud.com>
2023-08-06 15:55:22 +0200

    Add try, catch and finally

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,29 +9,37 @@ public class Main {
         // Welcome message
         System.out.println("Benvenuto! Adesso ti aiuterò a calcolare raggio, circonferenza e area dell tuo cerchio!");
 
-        // Ask for radius
-        System.out.println("Dimmi un po', quanti cm è lungo il raggio del tuo cerchio?");
-        double r = scanner.nextDouble();
-        scanner.nextLine();
+        try {
+            // Ask for radius
+            System.out.println("Dimmi un po', quanti cm è lungo il raggio del tuo cerchio?");
+            double r = scanner.nextDouble();
+            scanner.nextLine();
 
-        // Calculate diameter
-        double d = 2 * r;
+            // Calculate diameter
+            double d = 2 * r;
 
-        // Calculate circumference
-        double C = d * Math.PI;
+            // Calculate circumference
+            double C = d * Math.PI;
 
-        // Calculate area
-        double A = r * r * Math.PI;
+            // Calculate area
+            double A = r * r * Math.PI;
 
-        // Report printing
-        System.out.println("Bene, ecco a te i risultati:");
-        System.out.println("Diametro: " + d + " cm");
-        System.out.println("Circonferenza: " + C + " cm");
-        System.out.println("Area: " + A + " cm\u00B2");
+            // Report printing
+            System.out.println("Bene, ecco a te i risultati:");
+            System.out.println("Diametro: " + d + " cm");
+            System.out.println("Circonferenza: " + C + " cm");
+            System.out.println("Area: " + A + " cm\u00B2");
 
-        // Ending message
-        System.out.println("Spero di esserti stato utile, alla prossima!");
+            // Ending message
+            System.out.println("Spero di esserti stato utile, alla prossima!");
+        }
 
-        scanner.close();
+        catch(InputMismatchException e){
+            System.out.println("Per favore inserisci un numero.");
+        }
+
+        finally {
+            scanner.close();
+        }
     }
 }
